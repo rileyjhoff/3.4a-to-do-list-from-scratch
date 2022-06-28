76018970e50d2f33e2b987519213f0565e743546
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-28 15:50:31 -0700

    add test for POST /api/v1/users/sessions route, which should log in a user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -23,6 +23,23 @@ describe('users routes', () => {
     });
   });
 
+  it('POST /api/v1/users/sessions should log in a user', async () => {
+    // const res1 = await request(app).post('/api/v1/users/').send(testUser);
+
+    // expect(res1.status).toEqual(200);
+    // expect(res1.body).toEqual({
+    //   id: expect.any(Number),
+    //   email: testUser.email,
+    // });
+
+    const res2 = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(testUser);
+
+    expect(res2.status).toEqual(200);
+    expect(res2.body.message).toEqual('Signed in successfully');
+  });
+
   afterAll(() => {
     pool.end();
   });
