4a7997b931ebfd6b43f5071dba8db1c0d8583de6
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-28 14:29:31 -0700

    file starter code

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,13 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const testUser = {
+  email: 'test@test.com',
+  password: '123456',
+};
+
+describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('example test - delete me!', () => {
     expect(1).toEqual(1);
   });
+
   afterAll(() => {
     pool.end();
   });
