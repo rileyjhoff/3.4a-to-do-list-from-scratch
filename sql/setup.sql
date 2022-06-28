42aad41b8317266bc3419438419afb82f78a6f4b
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-28 14:13:47 -0700

    seed users table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
